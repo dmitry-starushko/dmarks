2254e40385169ee51bc2c012f0beb7a8b45480b7
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-09-26 16:43:58 +0300

    basic pages etc

--- markets/views.py
+++ markets/views.py
@@ -1,3 +1,15 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+class IndexView(View):
+    @property
+    def template_name(self):
+        return 'markets/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {
+            'page_title': 'Рынки Донбасса',
+            'body_class': 'index-page',
+        })
+
