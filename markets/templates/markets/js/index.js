2fdc34171ffa3334a4dfaed5157ac013e86dad67
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-09-26 17:16:12 +0300

    basic pages etc

---
+++ markets/templates/markets/js/index.js
@@ -0,0 +1,29 @@
+$(".foot-collapse").click(function(){
+        $(".collapse").collapse('toggle');
+        document.getElementById("close-footer").hidden = !document.getElementById("close-footer").hidden;
+    });
+    var map = new ol.Map({
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM({
+             url: 'https://opensm.donmarkets.ru/ru/{z}/{x}/{y}.png',
+             attributions:
+                '<a href="httsps://donmarkets.ru">Рынки Донбасса</a> карты' ,
+             crossOrigin: null
+          })
+       })
+     ],
+     target: 'map',
+     controls: ol.control.defaults({
+        attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+          collapsible: false
+        })
+     }),
+    view: new ol.View({
+       projection: 'EPSG:4326',
+       center: [37.902, 48.038],
+       zoom:11,
+       maxZoom:17,
+       minZoom:8
+    })
+ });
