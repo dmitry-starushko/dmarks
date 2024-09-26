24ffd16d1f4fffd31a0a7e65d457f46e8567e983
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-09-26 20:08:55 +0300

    basic pages etc

--- markets/views.py
+++ markets/views.py
@@ -26,4 +26,4 @@ class ContactsView(View, BasicContextProvider):
         return 'markets/contacts.html'
 
     def get(self, request):
-        return render(request, self.template_name, self.basic_context | {'page_title': 'Просто страница'})
+        return render(request, self.template_name, self.basic_context | {'page_title': 'Здесь будут контакты'})
