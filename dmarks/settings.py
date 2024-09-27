0cf1ea38de5856de3d5c3218a3bcefca6e2d8c63
Sergey <yelezov@gmail.com>
2024-09-27 15:53:22 +0300

    settings

--- dmarks/settings.py
+++ dmarks/settings.py
@@ -70,17 +70,6 @@ DATABASES = {
     }
 }
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'market',
-        'USER': 'market',
-        'PASSWORD': 'y5d2ZhChFm',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}"""
-
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
     'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer', 'rest_framework.renderers.BrowsableAPIRenderer'],
