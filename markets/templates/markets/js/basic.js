90abdaca64cae60e4660d7d5018a150253ac47b1
Sergey <yelezov@gmail.com>
2024-09-27 19:55:38 +0300

    basic.js, jquery remove

--- markets/templates/markets/js/basic.js
+++ markets/templates/markets/js/basic.js
@@ -5,6 +5,8 @@ function click_footer() {
     else {
         document.getElementById("footer-collapse").classList.add("show")
     }
-    //document.getElementById("footer-collapse").collapse('toggle');
     document.getElementById("close-footer").hidden = !document.getElementById("close-footer").hidden;
 };
+
+document.getElementById('about-btn').onclick = click_footer;
+document.getElementById('close-footer').onclick = click_footer;
--- markets/templates/markets/js/index.js
+++ markets/templates/markets/js/index.js
@@ -1,7 +1,4 @@
-$(".foot-collapse").click(function(){
-        $(".collapse").collapse('toggle');
-        document.getElementById("close-footer").hidden = !document.getElementById("close-footer").hidden;
-    });
+
     var map = new ol.Map({
       layers: [
         new ol.layer.Tile({
