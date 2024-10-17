e86e7f027693904a1aafea9fbe5419c11db925fc
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-10-18 00:45:43 +0300

    do not use transmutation

--- markets/api/views.py
+++ markets/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny
 from django.http.response import JsonResponse, HttpResponseBadRequest
 from markets.decorators import on_exception_returns
 from markets.models import SvgSchema
-from transmutation import Svg3DTM
+# from transmutation import Svg3DTM
 
 
 class TakeGltfView(APIView):
@@ -13,9 +13,9 @@ class TakeGltfView(APIView):
     @on_exception_returns(HttpResponseBadRequest)
     def get(request, scheme_pk: int):
         scheme = SvgSchema.objects.get(pk=scheme_pk)
-        svg3dtm = Svg3DTM()
-        r = JsonResponse({})
-        r.content = svg3dtm.transmutate(scheme.svg_schema)
-        return r
+        # svg3dtm = Svg3DTM()
+        response = JsonResponse({})
+        # response.content = svg3dtm.transmutate(scheme.svg_schema)
+        return response
 
 
