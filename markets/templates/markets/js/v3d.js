abf606d66cefc6e519e1a23eccc629f8d0bcaa67
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-10-18 05:01:20 +0300

    3d in progress

--- markets/templates/markets/js/v3d.js
+++ markets/templates/markets/js/v3d.js
@@ -1,1 +1,31 @@
-alert("Версия three.js: ".concat(THREE.REVISION, " (рисуем схему {{sch_pk}} в div #{{div_id}})"));
+{# alert("Версия three.js: ".concat(THREE.REVISION, " (рисуем схему {{sch_pk}} в div #{{div_id}})")); #}
+const width = window.innerWidth, height = window.innerHeight;
+
+// init
+
+const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( width, height );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+// animation
+
+function animate( time ) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
