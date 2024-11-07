e142aa7f49540b4287d011355ea83b7107d48108
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-11-07 03:44:28 +0300

    legend

--- markets/decorators.py
+++ markets/decorators.py
@@ -6,18 +6,20 @@ redis = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.RE
 
 
 def on_exception_returns(response_class, name=None):
+    """For API View`s methods. Defines response_class for response if some exceptions was raised.
+    Also, prevents too frequently calls of failing methods"""
     def decorator(function):
         qln = function.__qualname__
 
         def proxy(*args, **kwargs):
-            key = f'{qln}:{name}:{kwargs[name]}' if name is not None and name in kwargs else None
+            fail_flag = f'{qln}:{name}:{kwargs[name]}' if name is not None and name in kwargs else None
             try:
-                if key is not None and redis.get(key):
+                if fail_flag is not None and redis.exists(fail_flag):
                     raise ValueError(f'Не удалось обработать значение: {kwargs[name]}')
                 return function(*args, **kwargs)
             except Exception as e:
-                if key is not None:
-                    redis.set(name=key, value=1, ex=settings.FAIL_EXPIRE_SECONDS)
+                if fail_flag is not None:
+                    redis.set(name=fail_flag, value=1, ex=settings.FAIL_EXPIRE_SECONDS)
                 response = response_class()
                 response.content = str(e)
                 return response
@@ -26,6 +28,7 @@ def on_exception_returns(response_class, name=None):
 
 
 def globally_lonely_action(function):
+    """Creates system-wide "lonely" action like long-time database operation, etc."""
     gla_lock = Lock()
     gla_name = f'GLA:{function.__qualname__}:launched'
 
@@ -35,15 +38,13 @@ def globally_lonely_action(function):
 
     def proxy(*args, **kwargs):
         with gla_lock:
-            if redis.exists(gla_name):
+            if not redis.set(name=gla_name, value=1, ex=1800, nx=True):
                 return None
-            redis.set(name=gla_name, value=1, ex=1800)
         try:
             return function(*args, **kwargs)
         finally:
             with gla_lock:
                 redis.delete(gla_name)
-
     proxy.launched = launched
     return proxy
 
