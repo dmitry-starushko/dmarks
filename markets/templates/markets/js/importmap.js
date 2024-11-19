b32590a4fd9cc9e72948466b7ddb8792782429a7
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-11-19 14:51:03 +0300

    ts-chat

--- markets/templates/markets/js/importmap.js
+++ markets/templates/markets/js/importmap.js
@@ -1,7 +1,7 @@
 {% load static %}
 <script type="importmap"> {
     "imports": {
-        "ts_chat": "{% static 'js/ts-chat.module.js' %}"
+        "ts-chat": "{% static 'js/ts-chat.module.js' %}"
         ,"three": "{% static 'js/three/three.module.js' %}"
         ,"gltf_loader": "{% static 'js/three/loader/GLTFLoader.js' %}"
         ,"orbit_controls": "{% static 'js/three/controls/OrbitControls.js' %}"
