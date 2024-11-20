b3d76ac4ea49818d6a2ef014692ba6c560b7e594
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-11-20 15:16:37 +0300

    ts-chat

--- static/js/search.js
+++ static/js/search.js
@@ -1,8 +1,37 @@
 
 var searchbtn = document.getElementById('search-btn');
-var searchwindow = document.getElementById('search-market');
+var searchwindow = document.getElementById('search-market-window');
+var searchinput = document.getElementById('search-market-input');
+var globalTimeout = null;
+var maxsymbols = 3;
 
-searchbtn.addEventListener('click', function() {
-    //searchwindow.classList.toggle('hide');
-    alert('123');
-});
+var closebtn = document.getElementById("search-close");
+
+searchbtn.onclick = function() {
+  searchwindow.style.display = "block";
+}
+
+closebtn.onclick = function() {
+  searchwindow.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == searchwindow) {
+    searchwindow.style.display = "none";
+  }
+}
+
+
+searchinput.onkeyup = function() {
+  if (searchinput.value.length <= maxsymbols) {
+    return false;
+  }
+  if (globalTimeout != null) {
+    clearTimeout(globalTimeout);
+  }
+  globalTimeout = setTimeout(function() {
+    globalTimeout = null;
+    /* send request for data search */
+    alert(searchinput.value);
+  }, 300);
+}
