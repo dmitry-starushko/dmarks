431ece1e27fdf86a7680db1bc4154c79f213a37e
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-11-21 18:44:20 +0300

    ts-chat

--- markets/business/tech_support.py
+++ markets/business/tech_support.py
@@ -1,10 +1,24 @@
 from django.conf import settings
-from redis import Redis
+from asgiref.sync import async_to_sync
 from markets.telegram import Telegram
 import channels.layers
-from asgiref.sync import async_to_sync
+from redis import Redis
+
 
 redis = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+
+
+def send_message_to_ts(from_name, cids, message):
+    bot_id = settings.TELEBOT_ID
+    if bot_id:
+        si = Telegram(bot_id).send_message(set(cids), message)
+        msg_ids = set()
+        for cid in si:
+            if si[cid] and si[cid]['ok']:
+                msg_ids.add(si[cid]['result']['message_id'])
+        for mid in msg_ids:
+            key = f'questions:{mid}:from'
+            redis.set(name=key, value=from_name, ex=3600)
 
 
 def collect_messages_from_ts():
@@ -20,19 +34,3 @@ def collect_messages_from_ts():
                         redis.delete(key)
                         channel_layer = channels.layers.get_channel_layer()
                         async_to_sync(channel_layer.group_send)(from_name.decode(), {'type': 'message_from_ts', 'message': message['text']})
-
-
-def send_message_to_ts(from_name, cids, message):
-    collect_messages_from_ts()
-    bot_id = settings.TELEBOT_ID
-    if bot_id:
-        si = Telegram(bot_id).send_message(set(cids), message)
-        msg_ids = set()
-        for cid in si:
-            if si[cid] and si[cid]['ok']:
-                msg_ids.add(si[cid]['result']['message_id'])
-        for mid in msg_ids:
-            key = f'questions:{mid}:from'
-            redis.set(name=key, value=from_name, ex=3600)
-
-
--- markets/tasks.py
+++ markets/tasks.py
@@ -1,7 +1,18 @@
 import os
 from celery import shared_task
+from dmarks import celery
 from markets.business.actions import restore_db_consistency
-from markets.business.tech_support import send_message_to_ts
+from markets.business.tech_support import send_message_to_ts, collect_messages_from_ts
+
+
+@celery.app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(5.0, pt_collect_messages_from_ts.s(), name='collect messages')
+
+
+@celery.app.task
+def pt_collect_messages_from_ts():
+    collect_messages_from_ts()
 
 
 @shared_task
