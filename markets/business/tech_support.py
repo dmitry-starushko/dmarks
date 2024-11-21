a08ed7eba11dd89d6e8ac5dcd3feec13c965379e
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-11-21 21:09:06 +0300

    ts-chat

--- markets/business/tech_support.py
+++ markets/business/tech_support.py
@@ -13,24 +13,33 @@ def send_message_to_ts(from_name, cids, message):
     if bot_id:
         si = Telegram(bot_id).send_message(set(cids), f'{from_name}:\n{message}')
         msg_ids = set()
+        redis.keys();
         for cid in si:
             if si[cid] and si[cid]['ok']:
                 msg_ids.add(si[cid]['result']['message_id'])
         for mid in msg_ids:
             key = f'questions:{mid}:from'
-            redis.set(name=key, value=from_name, ex=3600)
+            redis.set(name=key, value=from_name, ex=1800)
 
 
 def collect_messages_from_ts():
     bot_id = settings.TELEBOT_ID
-    if bot_id:
-        updates = Telegram(bot_id).get_updates()
-        if updates and updates['ok']:
+    if bot_id and redis.keys("questions:*:from"):
+        updates = (tbot := Telegram(bot_id)).get_updates()
+        if updates is not None and updates['ok']:
             for upd in updates['result']:
-                if 'message' in upd and (message := upd['message']) is not None and 'reply_to_message' in message:
-                    r_mid = message['reply_to_message']['message_id']
-                    key = f'questions:{r_mid}:from'
-                    if (from_name := redis.get(key)) is not None:
-                        redis.delete(key)
-                        channel_layer = channels.layers.get_channel_layer()
-                        async_to_sync(channel_layer.group_send)(from_name.decode(), {'type': 'message_from_ts', 'message': message['text']})
+                if 'message' in upd and (message := upd['message']) is not None:
+                    if 'reply_to_message' in message:
+                        r_mid = message['reply_to_message']['message_id']
+                        key = f'questions:{r_mid}:from'
+                        if (from_name := redis.get(key)) is not None:
+                            redis.delete(key)
+                            channel_layer = channels.layers.get_channel_layer()
+                            async_to_sync(channel_layer.group_send)(from_name.decode(), {'type': 'message_from_ts', 'message': message['text']})
+                    else:
+                        w_mid = message['message_id']
+                        w_cid = message['from']['id']
+                        w_key = f'warnings:{w_cid}:{w_mid}'
+                        if not redis.exists(w_key):
+                            redis.set(name=w_key, value=1, ex=3600*72)
+                            tbot.send_message({message['from']['id']}, f'*Безадресные* сообщения *не допускаются*! Отвечайте на сообщения клиента!', True)
