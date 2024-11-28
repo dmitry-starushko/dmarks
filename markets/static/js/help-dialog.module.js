05709a63dd24714d13332d57637442fcb7e745ae
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-11-28 16:53:30 +0300

    in progress

--- markets/static/js/help-dialog.module.js
+++ markets/static/js/help-dialog.module.js
@@ -1,19 +1,17 @@
 class HelpDialog {
-    constructor(dialog_id, help_id) {
+    constructor(dialog_id) {
         this._dialog = document.getElementById(dialog_id);
-        this._help_id = help_id;
         if(!this._dialog) { throw `DOM element #${dialog_id} not found`; }
         this._state = "closed"
         this._dialog.addEventListener("toggle", e => { this._state = e.newState; });
     }
 
-    toggle() {
+    toggle(help_id) {
         if(this._state == "open") {
             this._dialog.close();
         } else {
-            dj_load_partial_view("partial_help_content", {hid: this._help_id}, {}).then(
+            dj_load_partial_view("partial_help_content", {hid: help_id}, {}).then(
                 html => {
-                    debugger;
                     this._dialog.getElementsByTagName("section").item(0).innerHTML = html;
                     this._dialog.showModal();
                 }
