5c12ae41c6d0196463e648ac12eb57ff38452432
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-12-14 15:47:43 +0300

    in progress

--- markets/static/js/search.module.js
+++ markets/static/js/search.module.js
@@ -3,6 +3,18 @@ class OutletFilters {
     }
 
     build_json() {
+        const checkboxes_2s = document.querySelectorAll("input:not([name='tricheckbox'])[type='checkbox'][role='outlet-filter']");
+        const checkboxes_3s = document.querySelectorAll("input[name='tricheckbox'][type='checkbox'][role='outlet-filter']");
+        const ranges = document.querySelectorAll("input[type='range'][role='outlet-filter']");
+        const filters = {
+            outlet_states: [],
+            specializations: [],
+            facilities: {},
+            price_range: {},
+            area_range: {}
+        };
+        // fill filters here
+        return JSON.stringify(filters);
     }
 }
 
