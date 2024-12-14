63c09912d96ad4ae392dd65fe8d2b8d69f598299
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-12-14 16:00:07 +0300

    in progress

--- markets/templates/markets/js/basic.js
+++ markets/templates/markets/js/basic.js
@@ -10,3 +10,6 @@ function click_footer() {
 
 document.getElementById('about-btn').onclick = click_footer;
 document.getElementById('close-footer').onclick = click_footer;
+window.ts_chat = new TsChat("ts-chat", "markets/ws/chat/", ("WebSocket" in window) ? "{{parm_ts_greeting}}" : "{{parm_ts_old_browser_warning}}");
+window.help_dialog = new HelpDialog("help-dialog");
+window.outlet_filters = new OutletFilters();
