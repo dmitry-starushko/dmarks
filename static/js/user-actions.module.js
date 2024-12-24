733eb4adda950197bced059a93fb3edd9bec292f
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-12-24 19:42:13 +0300

    in progress

--- static/js/user-actions.module.js
+++ static/js/user-actions.module.js
@@ -1,5 +1,29 @@
 class UserActions {
-    constructor() {
+    constructor(dialog_id) {
+        this._dialog = document.getElementById(dialog_id);
+        if(!this._dialog) { throw `DOM element #${dialog_id} not found`; }
+    }
+
+    __action__(action) {
+        dj_load_partial_view("partial_user_action", {}, action).then(
+            html => {
+                this._dialog.getElementsByTagName("section").item(0).innerHTML = html;
+                this._dialog.showModal();
+                this._dialog.style.opacity = "1.0";
+            }
+        );
+    }
+
+    book_outlet(number) {
+        this.__action__({action:"book-outlet", outlet: number});
+    }
+
+    unbook_all(number) {
+    }
+
+    close_dialog() {
+        this._dialog.style.opacity = "0.0";
+        window.setTimeout(()=>this._dialog.close(), 450);
     }
 }
 
