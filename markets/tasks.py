bf437bffc5ffb11247d99fe625faf079eb0e7653
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-12-27 19:18:43 +0300

    in progress

--- markets/tasks.py
+++ markets/tasks.py
@@ -1,14 +1,15 @@
 from celery import shared_task
 from dmarks import celery
-from markets.business.actions import restore_db_consistency, observe_all, delete_obsolete_notifications
+from markets.business.actions import restore_db_consistency, observe_all, delete_obsolete_notifications, logrotate
 from markets.business.tech_support import send_message_to_ts, collect_messages_from_ts
 
 
 @celery.app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(10.0, pt_collect_messages_from_ts.s(), name='collect messages')
+    sender.add_periodic_task(11.0, pt_collect_messages_from_ts.s(), name='collect messages')
     sender.add_periodic_task(3600.0, pt_observe_all.s(), name='observe all observables')
-    sender.add_periodic_task(12 * 3600.0, pt_delete_obsolete_notifications.s(), name='delete obsolete notifications')
+    sender.add_periodic_task(13 * 3600.0, pt_delete_obsolete_notifications.s(), name='delete obsolete notifications')
+    sender.add_periodic_task(17 * 3600.0, pt_delete_obsolete_notifications.s(), name='logrotate')
 
 
 @celery.app.task
@@ -19,6 +20,11 @@ def pt_collect_messages_from_ts():
 @celery.app.task
 def pt_observe_all():
     observe_all()
+
+
+@celery.app.task
+def pt_logrotate():
+    logrotate()
 
 
 @celery.app.task
