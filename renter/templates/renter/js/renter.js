89c8d6c15be294509439d376fcfca2f30b50083a
Sergey Yelezov <yelezov@gmail.com>
2024-12-30 12:22:23 +0300

    create LK

--- renter/templates/renter/js/renter.js
+++ renter/templates/renter/js/renter.js
@@ -1,2 +1,13 @@
 window.calendar = new Calendar("calendar-container");
 
+function sidebar_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("leftSidebar").style.width = "25%";
+  document.getElementById("leftSidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function side_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("leftSidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
