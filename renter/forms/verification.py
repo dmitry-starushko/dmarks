713072b5b535c6d91568ce8d2288969efd562b95
Dmitry Starushko <dmitry.starushko@gmail.com>
2025-01-03 01:41:15 +0300

    in progress

--- renter/forms/verification.py
+++ renter/forms/verification.py
@@ -1,7 +1,8 @@
 from django import forms
+from markets.validators import Validators
 
 
 class VerificationForm(forms.Form):
-    itn = forms.CharField(label='ИНН', min_length=10, max_length=12)
-    usr_le_extract = forms.FileField(label='Скан выписки из ЕСГРЮЛ')
-    passport_scan = forms.FileField(label='Скан паспорта')
+    itn = forms.CharField(label='ИНН', min_length=10, max_length=12, required=True, validators=[Validators.itn], widget=forms.TextInput(attrs={'pattern': Validators.ITN, 'placeholder': 'Число из 10 или 12 цифр'}))
+    usr_le_extract = forms.FileField(label='Скан выписки из ЕСГРЮЛ', max_length=1024, required=True, widget=forms.FileInput(attrs={'accept': 'image/*,.pdf'}))
+    passport_scan = forms.FileField(label='Скан паспорта', required=True, widget=forms.FileInput(attrs={'accept': 'image/*,.pdf'}))
