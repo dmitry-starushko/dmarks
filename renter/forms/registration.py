245756adf503902e2c7f11e39cf867d2fc83077d
Dmitry Starushko <dmitry.starushko@gmail.com>
2025-01-04 16:31:19 +0300

    in progress

--- renter/forms/registration.py
+++ renter/forms/registration.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from markets.models import DmUser
+from markets.validators import Validators
+import re
 
 
 class RegistrationForm(forms.ModelForm):
@@ -10,8 +13,25 @@ class RegistrationForm(forms.ModelForm):
         model = DmUser
         fields = ['phone', 'first_name', 'last_name', 'email']
 
+    def clean_phone(self):
+        cd = self.cleaned_data
+        try:
+            Validators.phone(cd['phone'])
+        except ValidationError:
+            raise forms.ValidationError('Укажите номер телефона в формате +7(999)999-99-99')
+        return cd['phone']
+
     def clean_password2(self):
-        cd = self.cleaned_data()
-        if cd['password'] != cd['password2']:
+        cd = self.cleaned_data
+        password = cd['password2']
+        if password != cd['password']:
             raise forms.ValidationError('Пароли не совпадают')
-        return cd['password2']
+        if len(password) < 8:
+            raise forms.ValidationError('Пароль должен содержать не менее 8 символов')
+        if re.search('\\d', password) is None:
+            raise forms.ValidationError('Пароль должен содержать хотя бы одну цифру')
+        if re.search('[A-ZА-ЯЁ]', password) is None:
+            raise forms.ValidationError('Пароль должен содержать хотя бы одну заглавную букву')
+        if re.search('[a-zа-яё]', password) is None:
+            raise forms.ValidationError('Пароль должен содержать хотя бы одну строчную букву')
+        return password
--- renter/urls.py
+++ renter/urls.py
@@ -6,6 +6,7 @@ app_name = 'renter'
 
 urlpatterns = [
     path('', RenterView.as_view(), name='renter'),
+    path('register/', RegistrationView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
