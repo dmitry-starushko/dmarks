820aa0985e764ce320cced1fc308b2a92e2a9a39
Dmitry Starushko <dmitry.starushko@gmail.com>
2025-01-14 18:02:11 +0300

    in progress

--- markets/business/booking.py
+++ markets/business/booking.py
@@ -16,7 +16,7 @@ def book_outlet(user: DmUser, outlet: TradePlace):
         raise BookingError('Для бронирования торгового места необходимо пройти процедуру верификации в личном кабинете')
     if outlet.trade_place_type.type_name != OutletState.AVAILABLE_FOR_BOOKING:
         raise BookingError(f'Статус торгового места {outlet.location_number}: {outlet.trade_place_type}')
-    dlog_info(user, f'Пользователь {user.phone} запросил бронирование ТМ {outlet.location_number}')
+    dlog_info(user, f'Пользователь {user.phone} инициировал заявку на бронирование ТМ {outlet.location_number}')
     with httpx.Client() as client:
         try:
             res = client.post(settings.EXT_URL['booking'].format(user=user.aux_data.itn),
@@ -26,6 +26,7 @@ def book_outlet(user: DmUser, outlet: TradePlace):
                 reason = f'{res.text or 'без пояснений'}'
                 dlog_warn(user, f'Пользователю {user.phone} отказано в бронировании {outlet.location_number}: {reason}')
                 raise BookingError(f'В бронировании отказано: {reason}')
+            dlog_info(user, f'Запрос пользователя {user.phone} на бронирование ТМ {outlet.location_number} отправлен')
             return True
         except httpx.TransportError as e:
             dlog_error(user, f'Запрос пользователя {user.phone} на бронирование {outlet.location_number} не удалось отправить: {e}')
@@ -50,11 +51,20 @@ def get_outlets_in_booking(user: DmUser):
 def unbook_all(user: DmUser):
     if not user.confirmed:
         raise RuntimeError(FUS.UNV)
+    dlog_info(user, f'Пользователь {user.phone} запросил аннулирование всех запросов на бронирование ТМ')
     with httpx.Client() as client:
-        res = client.delete(settings.EXT_URL['booking'].format(user=user.aux_data.itn))
-        if res.is_error:
-            raise RuntimeError(FUS.SRE)
-        result = res.json()
-    match result:
-        case [*items]: return frozenset(f'{i}' for i in items)
-        case _: raise RuntimeError(FUS.USR)
+        try:
+            res = client.delete(settings.EXT_URL['booking'].format(user=user.aux_data.itn))
+            if res.is_error:
+                reason = f'{res.text or 'без пояснений'}'
+                dlog_warn(user, f'Запрос пользователя {user.phone} на аннулирование всех запросов на бронирование ТМ отклонен сервером: {reason}')
+                raise RuntimeError(FUS.SRE)
+            result = res.json()
+            match result:
+                case [*items]:
+                    dlog_info(user, f'Запрос пользователя {user.phone} на аннулирование всех запросов на бронирование ТМ завершен успешно')
+                    return frozenset(f'{i}' for i in items)
+                case _: raise RuntimeError(FUS.USR)
+        except httpx.TransportError as e:
+            dlog_error(user, f'Запрос пользователя {user.phone} на аннулирование всех запросов на бронирование ТМ не удалось отправить: {e}')
+            raise RuntimeError(f'Ошибка сети: {e}') from e
