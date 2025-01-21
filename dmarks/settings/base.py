e268d1f70459a0d22bcf505bc90ff14f64f2b5b3
Dmitry Starushko <dmitry.starushko@gmail.com>
2025-01-21 23:31:03 +0300

    in progress

--- dmarks/settings/base.py
+++ dmarks/settings/base.py
@@ -78,6 +78,13 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'DMarks API',
+    'DESCRIPTION': 'API сайта Рынки Донбасса - Интерактивные карты',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
+
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
