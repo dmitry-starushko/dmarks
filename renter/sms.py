b334f41218d05afaa7707a99998471210a8671bd
Sergey Yelezov <yelezov@gmail.com>
2025-02-06 15:51:13 +0300

    Create sorting table with TM

--- renter/sms.py
+++ renter/sms.py
@@ -1,12 +1,29 @@
+import httpx
+from django.conf import settings
 from markets.decorators import on_exception_returns
 
 
 @on_exception_returns('@')
 def send_sms_code(phone: str) -> str:
-    return 'e39641da-7c2f-4cd9-9524-9ff785e56116'
+    with httpx.Client() as client:
+        res = client.post(settings.URLS_SMS_API['send'],
+              headers={'Content-Type': 'application/json'} | ({'Authorization': settings.AUTH_SMS_API} if settings.AUTH_SMS_API else {}),
+              json={"gatewayId": "1Gx5IJ", "channelType": "SMS", "destination": phone})
+        datares = res.json()
+        if res.is_error:
+            return "Сервис недоступный"
+        return datares['uuid']
+
 
 
 @on_exception_returns(False)
 def check_sms_code(code: str, uuid: str) -> bool:
     print(code, uuid)
-    return code == '12345'
+    with httpx.Client() as client:
+        res = client.post(settings.URLS_SMS_API['confirmation'],
+              headers={'Content-Type': 'application/json'} | ({'Authorization': settings.AUTH_SMS_API} if settings.AUTH_SMS_API else {}),
+              json={"uuid": uuid, "code": code})
+        datares = res.json()
+        if res.is_error:
+            return False
+    return datares['status'] == 'CONFIRMED'
