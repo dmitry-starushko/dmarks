bbaa8e41a577c92b862c162650237899ce8d7244
Dmitry Starushko <dmitry.starushko@gmail.com>
2025-02-18 15:32:24 +0300

    in progress

--- markets/templates/markets/js/basic.js
+++ markets/templates/markets/js/basic.js
@@ -1,16 +1,15 @@
-function click_footer() {
+const click_footer = () => {
     if (document.getElementById("footer-collapse").classList.contains("show")) {
         document.getElementById("footer-collapse").classList.remove("show")
-    }
-    else {
+    } else {
         document.getElementById("footer-collapse").classList.add("show")
     }
     document.getElementById("close-footer").hidden = !document.getElementById("close-footer").hidden;
 };
-
 document.getElementById('about-btn').onclick = click_footer;
 document.getElementById('close-footer').onclick = click_footer;
 window.ts_chat = new TsChat("ts-chat", "markets/ws/chat/", ("WebSocket" in window) ? "{{parm_ts_greeting}}" : "{{parm_ts_old_browser_warning}}");
 window.outlet_filters = new OutletFilters();
 window.help_dialog = new HelpDialog("help-dialog");
 window.user_actions = new UserActions('user-dialog');
+{% include 'markets/js/yandex.js' with code='98564520' %}
