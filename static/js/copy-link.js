c490a13ae6c4eb114e77257b51ac3daace22b65e
Dmitry Starushko <dmitry.starushko@gmail.com>
2025-02-24 09:01:34 +0300

    in progress

--- static/js/copy-link.js
+++ static/js/copy-link.js
@@ -1,7 +1,13 @@
 async function copy_absolute_url_by_id(link_id) {
+    if (navigator.clipboard) {
     const link_element = document.getElementById(link_id);
-    if (!link_element) { throw new Error(`Элемент с id "${link_id}" не найден.`); }
-    const relative_link = link_element.getAttribute('href');
-    const absolute_url = new URL(relative_link, window.location.href).toString();
-    await navigator.clipboard.writeText(absolute_url);
+        if (!link_element) { throw new Error(`Элемент с id "${link_id}" не найден.`); }
+        const relative_link = link_element.getAttribute("href");
+        const absolute_url = new URL(relative_link, window.location.href).toString();
+        await navigator.clipboard.writeText(absolute_url);
+    } else {
+        alert(`Упс, кажется, ваш браузер не поддерживает работу с буфером обмена.
+Не переживайте! Просто скопируйте ссылку вручную — это займет всего пару секунд.
+Спасибо за понимание!`);
+    }
 }
