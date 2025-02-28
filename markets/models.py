372f0b135815f82e392b1ca08bcb114bb5c5d67d
Dmitry Starushko <dmitry.starushko@gmail.com>
2025-02-28 18:37:21 +0300

    in progress

--- markets/models.py
+++ markets/models.py
@@ -15,13 +15,19 @@ from datetime import timezone, timedelta
 mtz = timezone(timedelta(hours=3))
 
 
+def replace_quotes(text):
+    def replace_match(match):
+        return '«' + match.group(1) + '»'
+    return re.sub(r'"([^"]*)"', replace_match, text)
+
+
 def fool_proof(text, replacement):
     if not hasattr(fool_proof, "statics"):
         fool_proof.statics = True
-        fool_proof.replacer = re.compile(r'[^а-яА-ЯёЁ0-9.,«»\- ]+')
+        fool_proof.replacer = re.compile(r'[^а-яА-ЯёЁ0-9.,«»"\- ]+')
         fool_proof.checker = re.compile(r'[а-яА-ЯёЁ]{2,}')
     text = fool_proof.replacer.sub('☒', text).strip()
-    return text if fool_proof.checker.search(text) else replacement
+    return replace_quotes(text) if fool_proof.checker.search(text) else replacement
 
 
 class DbItem(models.Model):
